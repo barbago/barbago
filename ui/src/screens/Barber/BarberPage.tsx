37c799007a173ea3198eb700217394d8220c5662
Hecker <julianheckerdev@gmail.com>
2022-04-21 23:46:20 -0400

    implement barber profile tab switching

--- ui/src/screens/Barber/BarberPage.tsx
+++ ui/src/screens/Barber/BarberPage.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ToggleButton } from 'react-native-paper';
 
 import { RootStackScreenProps } from '../../navigation';
 import { View, Text } from '../../components';
+import { CustomToggleButton } from './utils';
+import { ScrollView } from 'react-native';
 
 export const BarberPage = ({
   route,
   navigation,
 }: RootStackScreenProps<'Barber'>) => {
+  const [value, setValue] = useState('');
   useEffect(() => {
     navigation.setOptions({
       title: route.params.id,
@@ -16,6 +20,15 @@ export const BarberPage = ({
   return (
     <View>
       <Text>This is the Barber Page! Neat stuff coming soon!</Text>
+      <ToggleButton.Row
+        onValueChange={(value) => setValue(value)}
+        value={value}
+      >
+        <CustomToggleButton value="Info" icon="text" />
+        <CustomToggleButton value="Services" icon="briefcase" />
+        <CustomToggleButton value="Photos" icon="image-album" />
+        <CustomToggleButton value="Reviews" icon="star" />
+      </ToggleButton.Row>
     </View>
   );
 };
