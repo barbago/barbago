a7405779913ffd55dbb272aba66077eb5d4ed405
Hecker <julianheckerdev@gmail.com>
2022-04-27 18:30:51 -0400

    remove extra log

--- ui/src/screens/Barber/screens/BarberReviewPage.tsx
+++ ui/src/screens/Barber/screens/BarberReviewPage.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text } from '../../../components';
 import { VendorTabScreenProps } from '../../../navigation';
 
 export const BarberReviewPage = ({
   navigation,
 }: VendorTabScreenProps<'Reviews'>) => {
-  useEffect(() => {
-    console.log(navigation);
-  }, []);
 
   return (
     <View>
