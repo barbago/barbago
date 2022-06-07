c40e2b5f7cb31b993c6a4606f512bcec9ff5d845
Hecker <julianheckerdev@gmail.com>
2022-06-07 10:04:17 -0400

    document env stuff

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,4 +1,4 @@
 # https://stackoverflow.com/questions/60176044/how-do-i-use-an-env-file-with-github-actions
+# Save the result of `cat ui/.env | base64 -w 0` into GitHub Secret ENV_FILE
 
 echo $ENV_FILE | base64 --decode > ui/.env
-cat ui/.env
