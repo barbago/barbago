a9f25bd9b8fd96542fe08e70057a6ec8ddc05902
Hecker <julianheckerdev@gmail.com>
2022-06-06 21:15:24 -0400

    fix workflow env

--- .github/scripts/env.sh
+++ .github/scripts/env.sh
@@ -1,3 +1,4 @@
 # https://stackoverflow.com/questions/60176044/how-do-i-use-an-env-file-with-github-actions
 
-echo $ENV_FILE | base64 --decode > ui/.env.new
+echo $ENV_FILE | base64 --decode > ui/.env
+cat ui/.env
