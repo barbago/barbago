197cb26fd62fd27d54a8a18cdae275cf24375e71
Hecker <julianheckerdev@gmail.com>
2022-06-07 23:03:58 -0400

    add profile caption

---
+++ ui/src/screens/Barber/components/ProfileHeader/ProfileCaption.tsx
@@ -0,0 +1,39 @@
+import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Text } from '../../../../components';
+
+export const ProfileCaption = () => {
+  return (
+    <View style={styles.container} pointerEvents="none">
+      <LinearGradient
+        colors={['transparent', 'rgba(0,0,0,0.9)']}
+        style={styles.gradient}
+      />
+
+      <Text style={styles.title}>Michael's Vintage Barber Shop</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  gradient: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    left: 0,
+    height: 100,
+  },
+  container: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    left: 0,
+    padding: 16,
+  },
+  title: {
+    position: 'relative',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
