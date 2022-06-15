287a5ff3e8a49239bbb8a1591d5f045ee19f300d
Hecker <julianheckerdev@gmail.com>
2022-06-15 11:33:26 -0400

    fix bug where title vanishes

--- ui/src/screens/Barber/components/ProfileHeader/ProfileHeader.tsx
+++ ui/src/screens/Barber/components/ProfileHeader/ProfileHeader.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { View } from 'react-native';
 import { ProfileButtons, ProfileCaption, ProfileCarousel } from './';
 
 export const ProfileHeader = () => {
   return (
-    <>
+    <View>
       <ProfileCarousel />
       <ProfileCaption />
       <ProfileButtons />
-    </>
+    </View>
   );
 };
