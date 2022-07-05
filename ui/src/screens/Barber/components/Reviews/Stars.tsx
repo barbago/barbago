66e2babddbc45c91936283590826e3aece6604bf
Hecker <julianheckerdev@gmail.com>
2022-07-05 18:43:00 -0400

    add stars rating component

---
+++ ui/src/screens/Barber/components/Reviews/Stars.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TextStyle, View, ViewStyle } from 'react-native';
+
+import { Text } from '../../../../components';
+
+export interface StarsProps {
+  rating: number;
+  maxStars?: number;
+  style?: ViewStyle;
+  starStyle?: TextStyle;
+  goldStarStyle?: TextStyle;
+  greyStarStyle?: TextStyle;
+}
+
+export const Stars = ({
+  rating,
+  maxStars = 5,
+  goldStarStyle = { color: 'gold' },
+  greyStarStyle = { opacity: 0.5 },
+  style,
+  starStyle = { fontSize: 20, lineHeight: 20 },
+}: StarsProps) => {
+  // todo: update this so that half stars can render
+  // possibly render all 5 empty stars, and full stars on top
+  // and a fraction of the last star
+  return (
+    <View style={[{ flexDirection: 'row', flexWrap: 'wrap' }, style]}>
+      {[...Array(maxStars).keys()].map((i) =>
+        i < Math.round(rating) ? (
+          <Text style={[starStyle, goldStarStyle]}>★</Text>
+        ) : (
+          <Text style={[starStyle, greyStarStyle]}>★</Text>
+        ),
+      )}
+    </View>
+  );
+};
