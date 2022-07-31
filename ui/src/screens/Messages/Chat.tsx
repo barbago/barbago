bccfa7f92a828501aa18ecc99b00936e779f1272
Hecker <julianheckerdev@gmail.com>
2022-07-30 23:56:30 -0400

    add react native gifted chat

--- ui/src/screens/Messages/Chat.tsx
+++ ui/src/screens/Messages/Chat.tsx
@@ -1,33 +1,78 @@
-import React, { useEffect } from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 
-import { Text } from '../../components';
 import { RootStackScreenProps } from '../../navigation';
+import { useAuth } from '../../providers';
 import { messageApi } from '../../store';
+import { ChatListLoader } from './ChatListLoader';
+import { getChatName, getUsersFromChat } from './utils';
 
 export const Chat = ({
   navigation,
   route,
 }: RootStackScreenProps<'Chat'>) => {
-  const chatId = route.params.id;
+  const { id: chatId } = route.params;
+  const { user } = useAuth();
 
-  const { data: messages } = messageApi.useGetMessagesQuery(chatId);
+  const [sendMessage] = messageApi.useSendMessageMutation();
+  const { data: chats } = messageApi.useGetChatsQuery(user!.uid);
+  const {
+    data: messages,
+    isLoading,
+    isError,
+  } = messageApi.useGetMessagesQuery(chatId);
+
+  const chat = useMemo(
+    () => chats?.find((chat) => chat.id === chatId),
+    [chats, chatId],
+  );
+
+  const members = useMemo(
+    () => (chat ? getUsersFromChat(chat) : []),
+    [chat],
+  );
+
+  const onSend = useCallback(
+    async (messages: IMessage[]) => {
+      for (let message of messages) {
+        await sendMessage({
+          chatId,
+          uid: user?.uid,
+          text: message.text,
+        });
+      }
+    },
+    [chatId, user],
+  );
 
   useEffect(() => {
-    navigation.setOptions({
-      title: chatId ?? 'Loading Chat...',
-    });
-  }, [chatId]);
+    navigation.setOptions({ title: getChatName(chat, user?.uid) });
+  }, [chat]);
 
-  // useEffect(() => {
-  //   isError && navigation.replace('NotFound');
-  // }, [isError]);
+  useEffect(() => {
+    isError && navigation.replace('NotFound');
+  }, [isError]);
 
   return (
-    <View>
-      {messages?.map((message, i) => (
-        <Text key={i}>{message.text}</Text>
-      ))}
-    </View>
+    <>
+      {!isLoading ? (
+        <GiftedChat
+          messages={messages?.map(({ id, text, date, sender }) => ({
+            // _id: id,
+            _id: date + text,
+            text,
+            createdAt: new Date(date),
+            user: members.find(({ _id }) => _id === sender)!,
+          }))}
+          onSend={onSend}
+          user={members.find(({ _id }) => _id === user?.uid)}
+          alwaysShowSend={true}
+          infiniteScroll
+          loadEarlier
+        />
+      ) : (
+        <ChatListLoader style={{ flexDirection: 'column-reverse' }} />
+      )}
+    </>
   );
 };
