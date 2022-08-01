c485e4654812c742dcf030ec08d03328280b1bfd
Hecker <julianheckerdev@gmail.com>
2022-08-01 00:57:46 -0400

    fix typo

--- ui/src/utils/devices.ts
+++ ui/src/utils/devices.ts
@@ -3,4 +3,4 @@ import { Platform } from 'react-native';
 export const isWeb = () => Platform.OS === 'web';
 export const isAndroid = () => Platform.OS === 'android';
 export const isIOS = () => Platform.OS === 'ios';
-export const isMobile = () => isWeb() || isAndroid();
+export const isMobile = () => isIOS() || isAndroid();
