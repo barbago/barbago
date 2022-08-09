ac8f6c0866206f8b28d21fec83db0ed92082bc84
Hecker <julianheckerdev@gmail.com>
2022-08-08 23:02:39 -0400

    initial pass at notification

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,40 @@
 import AppLoading from 'expo-app-loading';
+import {
+  addNotificationReceivedListener,
+  addNotificationResponseReceivedListener,
+  getExpoPushTokenAsync,
+  requestPermissionsAsync,
+} from 'expo-notifications';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useCachedResources, useColorScheme } from './hooks';
 import { Navigation } from './navigation';
 import { ContextProvider } from './providers';
+import { isMobile } from './utils';
 
 export function App() {
   const resourcesLoaded = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const subscriptions = [
+      addNotificationReceivedListener((notif) => console.log(notif)),
+      addNotificationResponseReceivedListener((res) =>
+        console.log(res),
+      ),
+    ];
+    (async () => {
+      await requestPermissionsAsync();
+      const token = isMobile() && (await getExpoPushTokenAsync()).data;
+
+      token && console.log(token);
+    })();
+
+    return subscriptions.forEach((subscription) =>
+      subscription.remove(),
+    );
+  }, []);
 
   if (!resourcesLoaded) {
     return <AppLoading />;
